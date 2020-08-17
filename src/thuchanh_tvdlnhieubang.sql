449b9414e94fe21a6e15757c1c3070a191bfa8ae
huy8895 <huy8895@gmail.com>
2020-08-17 11:35:23 +0700

    commit

--- src/thuchanh_tvdlnhieubang.sql
+++ src/thuchanh_tvdlnhieubang.sql
@@ -6,3 +6,13 @@ from customers
 inner join payments p on customers.customerNumber = p.customerNumber
 where city = 'Las Vegas';
 
+select customers.customerName,customers.customerNumber,orders.orderNumber,status
+from customers
+left join orders
+on customers.customerNumber = orders.customerNumber;
+
+select customers.customerNumber, customers.customerName, orders.orderNumber, orders.status
+from customers
+    LEFT JOIN orders
+        on customers.customerNumber = orders.customerNumber
+where orderNumber is null;
