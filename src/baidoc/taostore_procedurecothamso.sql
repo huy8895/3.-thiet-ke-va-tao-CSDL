66a6ab6e4eed6dcc5089760a289dc688787bf14f
huy8895 <huy8895@gmail.com>
2020-08-19 14:50:40 +0700

    commit

---
+++ src/baidoc/taostore_procedurecothamso.sql
@@ -0,0 +1,26 @@
+show databases ;
+use 4_baitapluyentap;
+
+create table sumOfAll(
+    amount int
+);
+
+insert into sumOfAll values
+(100),
+(300),
+(200),
+(450),
+(500);
+
+select * from sumOfAll;
+
+delimiter //
+create procedure sp_checkvl(in vl1 int , out vl2 int)
+begin
+    set vl2 = (select amount from sumOfAll where amount = vl1);
+end //
+delimiter ;
+
+call sp_checkvl(50,@isPresent);
+
+select @isPresent;
